bceb0cb81799a4beffeadb123df7e11610a19361
Raul Juarez <raultcj@outlook.com>
2015-09-07 01:22:15 -0700

    Added error check for inputs.

--- Proyect_1/Proyect_1/MainWindow.xaml.cs
+++ Proyect_1/Proyect_1/MainWindow.xaml.cs
@@ -13,24 +13,70 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Proyect_1
-{
+namespace Proyect_1 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        //Mover a un nuevo objeto?
+
+        public int newLimit;
+        public int readyLimit;
+        public int waitingLimit;
+        public int quantum;
+        public int probability;
+
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void probNum_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-        {
-            if (int.Parse(probNum.Text) >= 0 && int.Parse(probNum.Text) <= 100)
-            {
-                MessageBox.Show("Funciona");
+        private void playButton_Click(object sender, RoutedEventArgs e) {
+            if (!errorCheck().Equals("Passed")) {
+                MessageBox.Show(errorCheck());
             }
+            else {
+                MessageBox.Show("Works");
+            }
+        }
+
+        private string errorCheck() {
+            bool isNumeric;
+
+            isNumeric = int.TryParse(probability_text.Text, out probability);
+            if (!isNumeric && probability >= 0 && probability <= 100) {
+                return "Please input integers in range only. (Parameters/Probability)";
+            }
+
+            isNumeric = int.TryParse(quantum_text.Text, out quantum);
+            if (!isNumeric && quantum > 0) {
+                return "Please input positive integers only. (Parameters/Quantum)";
+            }
+
+            isNumeric = int.TryParse(newLimit_text.Text, out newLimit);
+            if (!isNumeric && newLimit > 0) {
+                return "Please input positive integers only. (List Limit/New)";
+            }
+
+            isNumeric = int.TryParse(readyLimit_text.Text, out readyLimit);
+            if (!isNumeric && readyLimit > 0) {
+                return "Please input positive integers only. (List Limit/Ready)";
+            }
+
+            isNumeric = int.TryParse(waitingLimit_text.Text, out waitingLimit);
+            if (!isNumeric && waitingLimit > 0) {
+                return "Please input positive integers only. (List Limit/Waiting)";
+            }
+
+            if (algSelected.SelectedItem.Equals(null)) {
+                return "Please make a selection. (Parameters/Algorithm)";
+            }
+
+            if (delaySelected.SelectedItem.Equals(null)) {
+                return "Please make a selection. (Parameters/Delay)";
+            }
+
+            return "Passed";
         }
     }
 }
